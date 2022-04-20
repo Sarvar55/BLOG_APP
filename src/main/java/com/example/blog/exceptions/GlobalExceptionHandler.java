cf6fa61f932268213450b360ace905c5419f6385
Sarvar55 <sarvar.musazade@gmail.com>
2022-04-21 01:13:51 +0300

    User validation method software passed all tests

---
+++ src/main/java/com/example/blog/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.example.blog.exceptions;
+
+import com.example.blog.payloads.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @project: Blog
+ * @author: Sarvar55
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
+        String message = ex.getMessage();
+        ApiResponse response = new ApiResponse(message, false);
+        return new ResponseEntity<ApiResponse>(response, HttpStatus.NOT_FOUND);//status code 404
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgsValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        ex.getBindingResult().getAllErrors().stream().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errorResponse.put(fieldName, message);
+        });
+
+        return new ResponseEntity<Map<String, String>>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+}
